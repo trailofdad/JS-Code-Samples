0b1511a0cf73fc5a3e8514a55692f1811a6236a6
trailofdad <christian.hapgood@gmail.com>
2015-10-20 20:27:41 -0300

    trying to get keyup working

--- Assignment 1/scripts.js
+++ Assignment 1/scripts.js
@@ -6,3 +6,16 @@ document.getElementById("reset").addEventListener("click",function(){
 
     location.reload();
 });
+
+var edit = document.getElementsByClassName("edit");
+
+for (var i = 0; i < edit.length; i++ ) {
+    edit[i].addEventListener("keyup", function(){
+       if(isNaN(parseInt(edit[i].textContent)) == true)
+       {
+           alert('hey');
+       }
+    });
+}
+
+
