f68b204acc7e643f7a2e6e34064568d10a5d4205
trailofdad <christian.hapgood@gmail.com>
2015-10-26 21:06:55 -0300

    finally got the number validation working

--- Assignment 1/scripts.js
+++ Assignment 1/scripts.js
@@ -2,19 +2,34 @@
  * Created by inet2005 on 10/20/15.
  */
 
+"use strict";
+
 document.getElementById("reset").addEventListener("click",function(){
 
     location.reload();
 });
 
-var edit = document.getElementsByClassName("edit");
+window.addEventListener("load", function() {
+    var edit = document.getElementsByClassName("edit");
+    for (var i = 0; i < edit.length; i++) {
+        edit[i].addEventListener('keyup', function () {
+            if(numCheck(this.textContent) != false){
+                //add id to change the td border to green
+            }
+            else{
+                alert("Please enter a valid number");
+                this.textContent = "";
+            }
+        })
+    }
+});
 
-edit.addEventListener("keyup", checkNum());
-
-function checkNum(edit)
-{
-    if(isNaN(edit.textContent))
-    {
-        alert('nan');
+//number validation function
+function numCheck(element){
+    var numReg = /^[1-9]$/;
+    if(element != "") {
+        if (numReg.exec(element) == null) {
+            return false;
+        }
     }
 }
