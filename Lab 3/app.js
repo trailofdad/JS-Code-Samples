5a64bb5ca5ae09919dd98191c98e686bd15fc7d9
trailofdad <christian.hapgood@gmail.com>
2015-11-06 11:50:14 -0400

    added bower and angular

--- Lab 3/app.js
+++ Lab 3/app.js
@@ -1,19 +1,21 @@
 (function() {
-  var app = angular.module('gemStore', ['store-directives']);
+    var app = angular.module('gemStore', ['store-directives']);
 
-  app.controller('StoreController', function(){
-    var store = this;
-    store.products = [];
-    
-  });
+    app.controller('StoreController', ['$http', function($http){
+        var store = this;
+        store.products = [];
+        $http.get('store-products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
 
-  app.controller('ReviewController', function() {
-    this.review = {};
+    app.controller('ReviewController', function() {
+        this.review = {};
 
-    this.addReview = function(product) {
-      product.reviews.push(this.review);
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
 
-      this.review = {};
-    };
-  });
+            this.review = {};
+        };
+    });
 })();
