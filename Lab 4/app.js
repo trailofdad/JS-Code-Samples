63a8cab28c9e8c621d70886971518c151b0c2448
trailofdad <christian.hapgood@gmail.com>
2015-11-08 23:05:26 -0400

    controller, and models finished. fleshing out the function

--- Lab 4/app.js
+++ Lab 4/app.js
@@ -1,59 +1,75 @@
-var students = [];
+angular.module('labFour', [])
+    .controller('studentController', function($scope){
 
-function student(first_name, last_name, student_id) {
-    this.fName = first_name;
-    this.lName = last_name;
-    this.sId = student_id;
-    this.report = function(){
-        return this.fName + " " + this.lName + " " + this.sId;
-    };
-}
+        $scope.students = [];
 
-document.getElementById("myButton").addEventListener("click",function(){
-    //alert("got here");
-    var fName = document.getElementById("fName").value;
-    var lName = document.getElementById("lName").value;
-    var sId = document.getElementById("sId").value;
+        $scope.createStudent = function(){
+            $scope.student = $scope.newID + " " + $scope.newFname + " " + $scope.newLname;
+            $scope.students.push($scope.student);
+        };
 
-    //alert(fName);
-    //alert(lName);
+    });
 
-    var studentOne = new student(fName,lName,sId);
 
-    function check(){
-        for(var j = 0; j < students.length; j++)
-        {
-            if(students[j].sId == sId)
-            {
-                alert("This student ID has already been used!")
-                return false;
-                break;
-            }
-        }
-        return true;
-    }
+//, text: $scope.newFname, text: $scope.newLname}
 
-    var bool = check();
 
-    if (bool == true)
-    {
-        students.push(studentOne);
-        var li = document.createElement("li"); //create list item
-        var node = document.createTextNode(students[students.length - 1].report());
-        li.appendChild(node);
-        document.getElementById("list").appendChild(li);
-
-    }
-
-});// end click function
-
-document.getElementById("trigger").addEventListener("mouseover",function(){
-    var json = document.getElementById("json");
-    json.style.display = "block";
-    json.innerHTML = JSON.stringify(students);
-});
-
-document.getElementById("json").addEventListener("click",function(){
-    var json = document.getElementById("json");
-    json.style.display = "none";
-});
+//var students = [];
+//
+//function student(first_name, last_name, student_id) {
+//    this.fName = first_name;
+//    this.lName = last_name;
+//    this.sId = student_id;
+//    this.report = function(){
+//        return this.fName + " " + this.lName + " " + this.sId;
+//    };
+//}
+//
+//document.getElementById("myButton").addEventListener("click",function(){
+//    //alert("got here");
+//    var fName = document.getElementById("fName").value;
+//    var lName = document.getElementById("lName").value;
+//    var sId = document.getElementById("sId").value;
+//
+//    //alert(fName);
+//    //alert(lName);
+//
+//    var studentOne = new student(fName,lName,sId);
+//
+//    function check(){
+//        for(var j = 0; j < students.length; j++)
+//        {
+//            if(students[j].sId == sId)
+//            {
+//                alert("This student ID has already been used!")
+//                return false;
+//                break;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    var bool = check();
+//
+//    if (bool == true)
+//    {
+//        students.push(studentOne);
+//        var li = document.createElement("li"); //create list item
+//        var node = document.createTextNode(students[students.length - 1].report());
+//        li.appendChild(node);
+//        document.getElementById("list").appendChild(li);
+//
+//    }
+//
+//});// end click function
+//
+//document.getElementById("trigger").addEventListener("mouseover",function(){
+//    var json = document.getElementById("json");
+//    json.style.display = "block";
+//    json.innerHTML = JSON.stringify(students);
+//});
+//
+//document.getElementById("json").addEventListener("click",function(){
+//    var json = document.getElementById("json");
+//    json.style.display = "none";
+//});
