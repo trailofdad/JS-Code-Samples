70f5b7184785ea8f168d54f27879cb524d76242e
trailofdad <christian.hapgood@gmail.com>
2015-11-14 22:45:57 -0400

    Initial Commit. Excluding bower components

---
+++ Assignment 2/app.js
@@ -0,0 +1,43 @@
+angular.module('studentsApp',[])
+    .controller('studentController',function($scope){
+
+        $scope.students = []; // end of students array
+
+        $scope.newStudent = function(){
+
+            $scope.students.push({fName:$scope.firstName, lName:$scope.lastName, stuId:$scope.studentId});
+            $scope.firstName = '';
+            $scope.lastName = '';
+            $scope.studentId = '';
+        };
+
+        $scope.addNewStudent = function(){
+
+            if($scope.students.length == 0) {
+                $scope.newStudent();
+            }else{
+                var found = false;
+                angular.forEach($scope.students,function(student){
+                    if (student.stuId == $scope.studentId) {
+                        alert("Id has been taken");
+                        found = true;
+                    }
+                });
+                if(!found){
+                    $scope.newStudent();
+                }
+            }
+        };
+
+        $scope.showDiv = function(){
+            $scope.toShow = true;
+        };
+
+        $scope.hideDiv = function(){
+            $scope.toShow = false;
+        }
+
+
+    })
+
+;
